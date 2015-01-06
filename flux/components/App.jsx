c8a624013bfb6b446f214502670523395453660d
hipertracker <hipertracker@gmail.com>
2015-01-06 15:46:09 +0000

    fixed missing Menu components from last commit

--- flux/components/App.jsx
+++ flux/components/App.jsx
@@ -1,9 +1,10 @@
 define(function (require, exports, module) {
     var React = require('react'),
-        ReactRouter = require('react-router'),
-        Menu = require('jsx!flux/components/Menu');
+        ReactRouter = require('react-router');
 
     var RouteHandler = ReactRouter.RouteHandler;
+    var Link = ReactRouter.Link;
+
     var App = React.createClass({
         render() {
             var styles = {
@@ -11,7 +12,16 @@ define(function (require, exports, module) {
             };
             return (
                 <section style={styles}>
-                    <Menu/>
+                    <div className="ui menu">
+                        <Link to="home" className="orange item">
+                            <i className="home icon"></i>
+                            Home
+                        </Link>
+                        <Link to="about" className="orange item">
+                            <i className="bomb icon"></i>
+                            About
+                        </Link>
+                    </div>
                     <RouteHandler/>
                 </section>
             )
